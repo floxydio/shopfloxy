f6a8a1219352e5cea31eb37cb81830d814d851f0
floxydio <diooktar@gmail.com>
2022-12-25 20:54:40 +0700

    Initial Commit

---
+++ ShopFloxy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopFloxy
+//
+//  Created by Dio Rovelino on 25/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopFloxy/ShopFloxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopFloxyApp.swift
+//  ShopFloxy
+//
+//  Created by Dio Rovelino on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ShopFloxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
